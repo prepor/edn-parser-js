3da99a975bd9e383b6a17d6ee39a6b6b89a238e7
Andrey Rudenko <me@prepor.dev>
2024-03-29 16:13:51 +0100

    init

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-export const myPackage = (taco = ''): string => `${taco} from my package`;
+import { parse } from './parser';
+
+export type EDN =
+  | number
+  | null
+  | boolean
+  | string
+  | EDN[]
+  | Map<EDN, EDN>
+  | Set<EDN>
+  | { symbol: string; ns?: string }
+  | { keyword: string; ns?: string }
+  | { list: EDN[] };
+
+export const ednParseMulti = (s: string): EDN[] => {
+  return parse(s) as EDN[];
+};
+
+export const ednParse = (edn: string): EDN => {
+  return ednParseMulti(edn)[0];
+};
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,13 +1,128 @@
-import { myPackage } from '../src';
+import { ednParse, ednParseMulti, EDN } from '../src';
 
-describe('index', () => {
-  describe('myPackage', () => {
-    it('should return a string containing the message', () => {
-      const message = 'Hello';
+describe('parsing', () => {
+  describe('symbols', () => {
+    it('should parse a symbol', () => {
+      expect(ednParse('foo')).toEqual({ symbol: 'foo' });
+      expect(ednParse('foo|123<>+-')).toEqual({ symbol: 'foo|123<>+-' });
 
-      const result = myPackage(message);
+      expect(ednParse('bar/foo')).toEqual({ ns: 'bar', symbol: 'foo' });
 
-      expect(result).toMatch(message);
+      expect(ednParseMulti('-a1 a:#')).toEqual([
+        { symbol: '-a1' },
+        { symbol: 'a:#' },
+      ]);
+    });
+  });
+
+  describe('keywords', () => {
+    it('should parse a keyword', () => {
+      expect(ednParse(':foo')).toEqual({ keyword: 'foo' });
+
+      expect(ednParse(':bar/foo')).toEqual({ ns: 'bar', keyword: 'foo' });
+    });
+  });
+
+  describe('special symbols', () => {
+    it('should parse a special symbol', () => {
+      expect(ednParseMulti('true false nil')).toEqual([true, false, null]);
+    });
+  });
+
+  describe('integers', () => {
+    it('should parse a integer', () => {
+      expect(ednParseMulti('1 123 123N')).toEqual([1, 123, 123]);
+    });
+  });
+
+  describe('float', () => {
+    it('should parse a float', () => {
+      expect(ednParseMulti('1.123 1E4 1M')).toEqual([1.123, 10000, 1]);
+    });
+  });
+
+  describe('chars', () => {
+    it('should parse a char', () => {
+      expect(ednParseMulti('\\a \\tab \\newline')).toEqual([
+        { char: 'a' },
+        { char: '\t' },
+        { char: '\n' },
+      ]);
+
+      expect(ednParse('\\u03A9')).toEqual({ char: 'Ω' });
+    });
+  });
+
+  describe('strings', () => {
+    it('should parse a string', () => {
+      expect(ednParse('"hello world"')).toEqual('hello world');
+      expect(ednParse('"hello\nworld"')).toEqual('hello\nworld');
+    });
+  });
+
+  describe('comments', () => {
+    it('should skip comments', () => {
+      expect(ednParse(';123\n123;123')).toEqual(123);
+
+      expect(ednParse('#_ [1 2 3] 123 #_ (1,2,3)')).toEqual(123);
+    });
+  });
+
+  describe('lists', () => {
+    it('should parse a list', () => {
+      expect(ednParse('(1 2 3)')).toEqual({ list: [1, 2, 3] });
+
+      expect(ednParse('(1,2,3)')).toEqual({ list: [1, 2, 3] });
+    });
+  });
+
+  describe('vectors', () => {
+    it('should parse a vector', () => {
+      expect(ednParse('[1 2 3]')).toEqual([1, 2, 3]);
+    });
+  });
+
+  describe('maps', () => {
+    it('should parse a map', () => {
+      expect(ednParse('{1 2 3 4}')).toEqual(
+        new Map([
+          [1, 2],
+          [3, 4],
+        ])
+      );
+    });
+  });
+
+  describe('sets', () => {
+    it('should parse a set', () => {
+      expect(ednParse('#{1 2 3 }')).toEqual(new Set([1, 2, 3]));
+    });
+  });
+
+  describe('tags', () => {
+    it('should parse a tag', () => {
+      expect(ednParse('#foo 123')).toEqual({
+        tag: { symbol: 'foo' },
+        value: 123,
+      });
+
+      expect(ednParse('#foo/bar 123')).toEqual({
+        tag: { ns: 'foo', symbol: 'bar' },
+        value: 123,
+      });
+    });
+  });
+
+  describe('namespaced maps', () => {
+    it('should parse a namespaced map', () => {
+      expect(ednParse('#:foo{:bar 1 :_/bar 2 :baz/bar 3 "str" 4}')).toEqual(
+        new Map<EDN, EDN>([
+          [{ keyword: 'bar', ns: 'foo' }, 1],
+          [{ keyword: 'bar' }, 2],
+          [{ keyword: 'bar', ns: 'baz' }, 3],
+          ['str', 4],
+        ])
+      );
     });
   });
 });
